24f1599caf8055a9f4b4530e4e6983ed33ffdefd
Prakhar Nagore <prakharnagore111@gmail.com>
2024-09-15 20:13:34 +0530

    add logo and select box

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,9 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to pizza shop</h1>
-      <Button>Button</Button>
-    </main>
+    <>
+      <h1>Home</h1>
+    </>
   );
 }
