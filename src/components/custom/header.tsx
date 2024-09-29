6d44928ae7ccbcfd2e651e8bdf89231ee188eaf9
Prakhar Nagore <prakharnagore111@gmail.com>
2024-09-29 22:58:34 +0530

    add redux toolkit

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -6,9 +6,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../ui/select";
-import { Phone, ShoppingBasket } from "lucide-react";
+import { Phone } from "lucide-react";
 import { Button } from "../ui/button";
 import { Tenant } from "@/lib/types";
+import CartCounter from "./cart-counter";
 
 const Header = async () => {
   const tenantsResponse = await fetch(
@@ -78,14 +79,7 @@ const Header = async () => {
               </Link>
             </li>
           </ul>
-          <div className="relative">
-            <Link href={"/cart"}>
-              <ShoppingBasket className="hover:text-primary" />
-            </Link>
-            <span className="absolute -top-4 -right-5 h-6 w-6 flex items-center justify-center rounded-full bg-primary font-bold text-white">
-              3
-            </span>
-          </div>
+          <CartCounter />
           <div className="flex items-center ml-12">
             <Phone />
             <span>+91 6262 726 352</span>
