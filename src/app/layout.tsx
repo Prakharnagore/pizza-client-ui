cdf2ac89679e5f14c9e45e0f69f0e581e01f7893
Prakhar Nagore <prakharnagore@Prakhars-MacBook-Air.local>
2025-10-05 22:28:54 +0530

    add auto token refresh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import Header from "@/components/custom/header";
 import StoreProvider from "./StoreProvider";
 import { Toaster } from "@/components/ui/toaster";
+import Refresher from "@/components/custom/refresher";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -30,9 +31,11 @@ export default function RootLayout({
             manrope.variable
           )}
         >
-          <Header />
-          <main>{children}</main>
-          <Toaster />
+          <Refresher>
+            <Header />
+            <main>{children}</main>
+            <Toaster />
+          </Refresher>
         </body>
       </StoreProvider>
     </html>
